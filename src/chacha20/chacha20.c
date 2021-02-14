f2a2dd98866418bd6c6eb3608a9b39f639df1e9d
rowanG077 <goemansrowan@gmail.com>
2021-02-14 17:37:35 +0100

    Placed buffers in CCM.

--- src/chacha20/chacha20.c
+++ src/chacha20/chacha20.c
@@ -7,21 +7,18 @@
 
 #define ROUNDS 20
 
-uint32_t lel __attribute__((section(".ccm_data"))) = 693321242;
-
-typedef uint32_t uint32;
-
-static uint32 load_littleendian(const unsigned char *x)
+__attribute__((section(".ccm")))
+static uint32_t load_littleendian(const unsigned char *x)
 {
-  lel += 1;
   return
-      (uint32) (x[0]) \
-  | (((uint32) (x[1])) << 8) \
-  | (((uint32) (x[2])) << 16) \
-  | (((uint32) (x[3])) << 24);
+      (uint32_t) (x[0]) \
+  | (((uint32_t) (x[1])) << 8) \
+  | (((uint32_t) (x[2])) << 16) \
+  | (((uint32_t) (x[3])) << 24);
 }
 
-static void store_littleendian(unsigned char *x,uint32 u)
+__attribute__((section(".ccm")))
+static void store_littleendian(unsigned char *x, uint32_t u)
 {
   x[0] = u; u >>= 8;
   x[1] = u; u >>= 8;
@@ -29,15 +26,17 @@ static void store_littleendian(unsigned char *x,uint32 u)
   x[3] = u;
 }
 
-static uint32 rotate(uint32 a, int d)
+__attribute__((section(".ccm")))
+static uint32_t rotate(uint32_t a, int d)
 {
-  uint32 t;
+  uint32_t t;
   t = a >> (32-d);
   a <<= d;
   return a | t;
 }
 
-static void quarterround(uint32 *a, uint32 *b, uint32 *c, uint32 *d)
+__attribute__((section(".ccm")))
+static void quarterround(uint32_t *a, uint32_t *b, uint32_t *c, uint32_t *d)
 {
   *a = *a + *b;
   *d = *d ^ *a;
@@ -56,6 +55,9 @@ static void quarterround(uint32 *a, uint32 *b, uint32 *c, uint32 *d)
   *b = rotate(*b, 7);
 }
 
+static uint32_t x[16] __attribute__((section(".ccm_data"))) = { 0 };
+static uint32_t j[16] __attribute__((section(".ccm_data"))) = { 0 };
+
 __attribute__((section(".ccm")))
 static int crypto_core_chacha20(
         unsigned char *out,
@@ -64,82 +66,81 @@ static int crypto_core_chacha20(
   const unsigned char *c
 )
 {
-  uint32 x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15;
-  uint32 j0, j1, j2, j3, j4, j5, j6, j7, j8, j9, j10, j11, j12, j13, j14, j15;
   int i;
 
-  j0  = x0  = load_littleendian(c +  0);
-  j1  = x1  = load_littleendian(c +  4);
-  j2  = x2  = load_littleendian(c +  8);
-  j3  = x3  = load_littleendian(c + 12);
-  j4  = x4  = load_littleendian(k +  0);
-  j5  = x5  = load_littleendian(k +  4);
-  j6  = x6  = load_littleendian(k +  8);
-  j7  = x7  = load_littleendian(k + 12);
-  j8  = x8  = load_littleendian(k + 16);
-  j9  = x9  = load_littleendian(k + 20);
-  j10 = x10 = load_littleendian(k + 24);
-  j11 = x11 = load_littleendian(k + 28);
-  j12 = x12 = load_littleendian(in+  8);
-  j13 = x13 = load_littleendian(in+ 12);
-  j14 = x14 = load_littleendian(in+  0);
-  j15 = x15 = load_littleendian(in+  4);
+  j[0]  = x[0]  = load_littleendian(c +  0);
+  j[1]  = x[1]  = load_littleendian(c +  4);
+  j[2]  = x[2]  = load_littleendian(c +  8);
+  j[3]  = x[3]  = load_littleendian(c + 12);
+  j[4]  = x[4]  = load_littleendian(k +  0);
+  j[5]  = x[5]  = load_littleendian(k +  4);
+  j[6]  = x[6]  = load_littleendian(k +  8);
+  j[7]  = x[7]  = load_littleendian(k + 12);
+  j[8]  = x[8]  = load_littleendian(k + 16);
+  j[9]  = x[9]  = load_littleendian(k + 20);
+  j[10] = x[10] = load_littleendian(k + 24);
+  j[11] = x[11] = load_littleendian(k + 28);
+  j[12] = x[12] = load_littleendian(in+  8);
+  j[13] = x[13] = load_littleendian(in+ 12);
+  j[14] = x[14] = load_littleendian(in+  0);
+  j[15] = x[15] = load_littleendian(in+  4);
 
   for (i = ROUNDS;i > 0;i -= 2) {
-    quarterround(&x0, &x4, &x8,&x12);
-    quarterround(&x1, &x5, &x9,&x13);
-    quarterround(&x2, &x6,&x10,&x14);
-    quarterround(&x3, &x7,&x11,&x15);
-    quarterround(&x0, &x5,&x10,&x15);
-    quarterround(&x1, &x6,&x11,&x12);
-    quarterround(&x2, &x7, &x8,&x13);
-    quarterround(&x3, &x4, &x9,&x14);
+    quarterround(&x[0], &x[4], &x[8],&x[12]);
+    quarterround(&x[1], &x[5], &x[9],&x[13]);
+    quarterround(&x[2], &x[6],&x[10],&x[14]);
+    quarterround(&x[3], &x[7],&x[11],&x[15]);
+    quarterround(&x[0], &x[5],&x[10],&x[15]);
+    quarterround(&x[1], &x[6],&x[11],&x[12]);
+    quarterround(&x[2], &x[7], &x[8],&x[13]);
+    quarterround(&x[3], &x[4], &x[9],&x[14]);
   }
 
-  x0 += j0;
-  x1 += j1;
-  x2 += j2;
-  x3 += j3;
-  x4 += j4;
-  x5 += j5;
-  x6 += j6;
-  x7 += j7;
-  x8 += j8;
-  x9 += j9;
-  x10 += j10;
-  x11 += j11;
-  x12 += j12;
-  x13 += j13;
-  x14 += j14;
-  x15 += j15;
+  x[0]+= j[0];
+  x[1]+= j[1];
+  x[2]+= j[2];
+  x[3]+= j[3];
+  x[4]+= j[4];
+  x[5]+= j[5];
+  x[6]+= j[6];
+  x[7]+= j[7];
+  x[8]+= j[8];
+  x[9]+= j[9];
+  x[10] += j[10];
+  x[11] += j[11];
+  x[12] += j[12];
+  x[13] += j[13];
+  x[14] += j[14];
+  x[15] += j[15];
 
-  store_littleendian(out + 0,x0);
-  store_littleendian(out + 4,x1);
-  store_littleendian(out + 8,x2);
-  store_littleendian(out + 12,x3);
-  store_littleendian(out + 16,x4);
-  store_littleendian(out + 20,x5);
-  store_littleendian(out + 24,x6);
-  store_littleendian(out + 28,x7);
-  store_littleendian(out + 32,x8);
-  store_littleendian(out + 36,x9);
-  store_littleendian(out + 40,x10);
-  store_littleendian(out + 44,x11);
-  store_littleendian(out + 48,x12);
-  store_littleendian(out + 52,x13);
-  store_littleendian(out + 56,x14);
-  store_littleendian(out + 60,x15);
+  store_littleendian(out + 0,  x[0]);
+  store_littleendian(out + 4,  x[1]);
+  store_littleendian(out + 8,  x[2]);
+  store_littleendian(out + 12, x[3]);
+  store_littleendian(out + 16, x[4]);
+  store_littleendian(out + 20, x[5]);
+  store_littleendian(out + 24, x[6]);
+  store_littleendian(out + 28, x[7]);
+  store_littleendian(out + 32, x[8]);
+  store_littleendian(out + 36, x[9]);
+  store_littleendian(out + 40, x[10]);
+  store_littleendian(out + 44, x[11]);
+  store_littleendian(out + 48, x[12]);
+  store_littleendian(out + 52, x[13]);
+  store_littleendian(out + 56, x[14]);
+  store_littleendian(out + 60, x[15]);
 
   return 0;
 }
 
-static const unsigned char sigma[16] = "expand 32-byte k";
+static const unsigned char __attribute__((section(".ccm_rodata"))) sigma[16] = "expand 32-byte k";
+static unsigned char in[16] __attribute__((section(".ccm_data"))) = { 0 };
+static unsigned char block[64] __attribute__((section(".ccm_data"))) = { 0 };
+static unsigned char kcopy[32] __attribute__((section(".ccm_data"))) = { 0 };
 
+__attribute__((section(".ccm")))
 int crypto_stream_chacha20(unsigned char *c,unsigned long long clen, const unsigned char *n, const unsigned char *k)
 {
-  unsigned char in[16];
-  unsigned char block[64];
-  unsigned char kcopy[32];
   unsigned long long i;
   unsigned int u;
 
